34decfafafbda5d427c3e95811aba2ed848f7e00
Narayan Zade <narayan.d.zade@gmail.com>
2026-03-05 10:35:16 +0530

    updates project structure

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routers import user
+from app.routers import main
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(user.router)
+app.include_router(main.router)
--- app/routers/main.py
+++ app/routers/main.py
@@ -8,8 +8,3 @@ router = APIRouter()
 @router.get("/")
 def get_users():
     return {"message": "Welcome to the User API"}
-
-
-@router.get("/users")
-def get_users():
-    return {"message": "List of users"}
